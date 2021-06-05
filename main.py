30ca83e743a44d57b0affd6fac56d39618d98e94
Ayan Rizvi <ayanrizvi786786@gmail.com>
2021-06-06 00:41:40 +0530

    sixth

--- main.py
+++ main.py
@@ -16,6 +16,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+from webdriver_manager.chrome import ChromeDriverManager
+
 
 app = Flask(__name__)
 
@@ -53,14 +55,6 @@ def get_contact():
 
 @app.route("/automate")
 def webscrap():
-    chrome_options = Options()
-    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--headless")
-    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
-
 
     GOOGLE_FORM_LINK = "https://forms.gle/u4VijuqdJdbtYjVN9"
 
@@ -93,7 +87,7 @@ def webscrap():
     print(all_link)
     print(all_address)
 
-
+    driver = webdriver.Chrome(ChromeDriverManager().install())
     driver.get(GOOGLE_FORM_LINK)
     time.sleep(5)
 
