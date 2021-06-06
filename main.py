f876c0132f5034a7a2b70e46abba742d446a0b11
Ayan Rizvi <ayanrizvi786786@gmail.com>
2021-06-06 11:00:10 +0530

    sixth

--- main.py
+++ main.py
@@ -112,17 +112,12 @@ def webscrap():
 
 @app.route("/automate2")
 def instabot():
-    chrome_options = Options()
-    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 
-
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument('--headless')
-    driver_path = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
     SIMILAR_ACCOUNT = "burakozcivit"
     USERNAME = "9889602245"
     PASSWORD = "aliraqqu"
+
+    driver_path = webdriver.Chrome(ChromeDriverManager().install())
 
     class InstaFollower:
 
